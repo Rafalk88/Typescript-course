213e59a6f5b5d9d1a2123c0326db051d7c73228e
Rafalk88 <99990595+Rafalk88@users.noreply.github.com>
2022-08-14 18:04:29 +0200

    primitive types

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,28 @@
 console.log('Działający TS!')
+
+// Primitive values
+
+// Error: 'age' implicitly has a type of 'any', but a better type can be inferred from its usage .ts(7043)
+// type 'any'
+// let age
+
+/*
+if not defined type, TS will define it as first type that is added to variable
+in this case type of age is 'number' and if another type will be added, there will
+be and Error
+*/
+//let age = 33
+
+let age: number = 33
+
+age = 34
+age = 34.5 // no Error
+
+// Error: Type 'string' is not assignable to type 'number' .ts(2322)
+// age = 'trzydzieści trzy'
+
+console.log(age)
+
+let ageAsString: string = 'trzydzieści trzy' // '' lub "" bez znaczenia, którego znacznika użyjemy
+
+ageAsString = `${age}` // ES6 no Error
